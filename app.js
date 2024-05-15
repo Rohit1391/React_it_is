a09f61a2a2467852e671942740203f0ac1524789
rohit1391 <2021uee1391@mnit.ac.in>
2024-05-15 17:04:42 +0530

    A small change!

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading=React.createElement(
     "h1", { id:"ahead" }, "Hello world from React!...");
     const parent=React.createElement(
         "div", {id:"parent"}, React.createElement(
             "div", {id:"child"}, [React.createElement(
                 "h1", {}, "Hii bey"), React.createElement(
-                    "h2", {}, "Hii bey")]
+                    "h2", {}, "Hii beby")]
             )
+        ); 
+        //const Heading =(<h1>Hii using JSX! </h1>);
+        //React functional component:
+        const Headingcomp=() => {
+            return <h1>Hey this is functional component!</h1>
+        };
+        //Another way for the same:
+        //This is component composition i.e combining two components in one!
+        const Headingcomp2= ()=>
+        (
+            <div>
+                <Headingcomp/>
+                {Headingcomp()} 
+               
+                
+            <h1> This is an another way! </h1>
+            </div>
+            
         );
+        
     
     const roomt=ReactDOM.createRoot(document.getElementById("root"));
     roomt.render(parent);
+    roomt.render(Headingcomp2());
+    //roomt.render(Heading);
